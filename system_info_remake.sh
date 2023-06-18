f41a50c335b1bc3a21c50176f4b65c6f3f48a00f
V <104692696+Roctic@users.noreply.github.com>
2023-06-17 23:00:38 -0300

    Uname simples
    Colocando em prática o uso do case com Shell Script, nessa demonstração utilizo as diversas opções do uname para testar os casos...

---
+++ system_info_remake.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#Um erro que me custou horas, esqueci de acessar a variável usando $, não cometa isso denovo
+
+echo "Select a letter():"
+echo "a all information"
+echo "m machine hardware name"
+echo "n network node hostname"
+echo "p processor type (non-portable)"
+echo "i hardware platform (non-portable)"
+echo "o operating system"
+echo "s kernel name"
+echo "v kernel version"
+echo "r kernel release"
+echo "q quit"
+
+read user_input;
+
+case $user_input in
+  "a")
+    uname -a
+    ;;
+  "m")
+    uname -m
+    ;;
+  "n")
+    uname -n
+    ;;
+  "p")
+    uname -p
+    ;;
+  "i")
+    uname -i
+    ;;
+  "o")
+    uname -o
+    ;;
+  "s")
+    uname -s
+    ;;
+  "v")
+    uname -v
+    ;;
+  "r")
+    uname -r
+    ;;
+  "q")
+    echo "Exiting..."
+    exit 0
+    ;;   
+esac
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
